412ebd49a0284cb00547f70778378b33c62fbb3a
nagendra9199 <nagendranaidu545@gmail.com>
2023-03-06 07:26:03 +0000

    Congrats Card

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div class="main-bg">
+    <h1>Congratulations</h1>
+    <div class="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <br />
+      <p>Vishnu Institute of computr Education and Technology Bhimavaram.</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
